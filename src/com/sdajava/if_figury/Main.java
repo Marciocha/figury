313389309d5f177b3a077cccf762b80cf6253583
Marciocha <taleja555@interia.pl>
2017-03-04 12:34:54 +0100

    add final files

---
+++ src/com/sdajava/if_figury/Main.java
@@ -0,0 +1,12 @@
+package com.sdajava.if_figury;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Pola kwadrat = new Pola (a:2, b: 5);
+
+        System.out.println(kwadrat.poleKwadratu());
+    }
+}
---
+++ src/com/sdajava/if_figury/Pola.java
@@ -0,0 +1,16 @@
+package com.sdajava.if_figury;
+
+/**
+ * Created by RENT on 2017-02-20.
+ */
+public class Pola {
+   public Pola(int a, int b) {
+       this.a = a;
+       this.b = b;
+   }
+   //konstruktor bezparametrowy
+
+    public int poleKwadratu()
+   }
+
+}
